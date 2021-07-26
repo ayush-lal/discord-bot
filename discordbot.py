80beb196925343ca02657201c31b150f5bec4fb5
ayush-lal <ayushlal12@gmail.com>
2021-07-26 20:55:54 +1000

    test commit

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,9 @@
 import requests
 import json
 import discord
-# From dotenv import load_dotenv
 from dotenv import load_dotenv
 load_dotenv()
 import os
-
-
-# app id: 642159110384779286
-# public key: efa9241751b1afd235003be73c7ab814067da4255ae1132ff6c505769f410d55
 
 DISCORD_TOKEN = os.environ.get('TOKEN')
 
@@ -18,7 +13,6 @@ response = requests.request("GET", url)
 data = response.json()
 memeUrl = data["url"]
 
-# TOKEN = 'NjQyMTU5MTEwMzg0Nzc5Mjg2.XcS3Rg.uIjgeJRPp5xo5U80Rb_eX9bakFY'
 GUILD = 'YTB'
 
 client = discord.Client()
